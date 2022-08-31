bfad558cb6a7d35048d15d786deee6565d98eede
vladimir <vladimir.hodaryonok>
2022-08-31 17:49:42 +0300

    fix: pr comments #2

--- lib/di/injector.dart
+++ lib/di/injector.dart
@@ -1,10 +1,11 @@
+// ignore: depend_on_referenced_packages
 import 'package:domain/domain.dart';
 import 'package:get_it/get_it.dart';
 import 'package:presentation/presentation.dart';
 
 final getIt = GetIt.instance;
 
-Future<void> configureDependencies() async {
+ void configureDependencies() {
   configurePresentationDependencies(getIt);
   configureDomainDependencies(getIt);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:guess_the_number/di/injector.dart';
 import 'package:presentation/presentation.dart';
 
-void main() async{
+void main() {
   configureDependencies();
   runApp(const MyApp());
 }
